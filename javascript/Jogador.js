78f10082f328e99e050b4efc8b846d48ae17399d
Flamma <flamma.lux@gmail.com>
2024-05-31 12:06:14 -0300

    CSS: alterado o número de colunas do grid do jogador | html: adicionados os botoes para teste, no primeiro jogador, de adicao e subtracao de pontuacao | Javascript: na classe Jogador foram criados as funcoes AlteraAvanco e AlteraPontuacao e foram atualizadas as funcoes AumentarPontuacao e DiminuirPontuacao

--- javascript/Jogador.js
+++ javascript/Jogador.js
@@ -1,19 +1,22 @@
 class Jogador {
     constructor () {
+        this.idJogador = "jogador-vermelho-1";
         this.numeroJogador = 0;
         this.pontuacaoJogador = 0;
         this.avancoJogador = 0;
         this.situacaoJogador = 0;
-        this.time = 0;
+        this.timeJogador = 0;
     }
 
-    AumentarPontuacao () {
+    AumentarPontuacao() {
         if(this.pontuacaoJogador < 5) {
             this.pontuacaoJogador += 1;
-            this.avancoJogador += 1;
             if (this.pontuacaoJogador == 4) {
                 this.pontuacaoJogador = 5;
             }
+            this.AlteraPontuacao(); //altera a pontuação do jogador
+            this.avancoJogador += 1;
+            this.AlteraAvanco(); //altera o avanço do jogador
         }else {
             console.log("Valor máximo de pontuação obtido");
         }
@@ -23,18 +26,62 @@ class Jogador {
     DiminuirPontuacao() {
         if(this.pontuacaoJogador > 0) {
             this.pontuacaoJogador -= 1;
-            this.avancoJogador -= 1;
             if (this.pontuacaoJogador == 4) {
                 this.pontuacaoJogador = 3;
             }
+            this.AlteraPontuacao(); //altera a pontuação do jogador
+            this.avancoJogador -= 1;
+            this.AlteraAvanco(); //altera o avanço do jogador
         }else {
             console.log("Valor mínimo de pontuação obtido");
         }
         return;
     }
 
+    AlteraPontuacao() {
+        const areaTexto = document.querySelector("#jogador-vermelho-1").querySelector('.jogador_pontuacao');
+        areaTexto.innerHTML = this.pontuacaoJogador;
+    }
+
+    AlteraAvanco() {
+        const arrayAvanco = document.querySelector("#jogador-vermelho-1").querySelectorAll(".jogador_avanco");
+        switch (this.avancoJogador) {
+            case 1:
+                arrayAvanco[0].classList.add("ativo");
+                arrayAvanco[1].classList.remove("ativo");
+                arrayAvanco[2].classList.remove("ativo");
+                arrayAvanco[3].classList.remove("ativo");
+                break;
+
+            case 2:
+                arrayAvanco[0].classList.add("ativo");
+                arrayAvanco[1].classList.add("ativo");
+                arrayAvanco[2].classList.remove("ativo");
+                arrayAvanco[3].classList.remove("ativo");
+                break;
+
+            case 3:
+                arrayAvanco[0].classList.add("ativo");
+                arrayAvanco[1].classList.add("ativo");
+                arrayAvanco[2].classList.add("ativo");
+                arrayAvanco[3].classList.remove("ativo");
+                break;
+                
+            case 4:
+                arrayAvanco[0].classList.add("ativo");
+                arrayAvanco[1].classList.add("ativo");
+                arrayAvanco[2].classList.add("ativo");
+                arrayAvanco[3].classList.add("ativo");
+                break;
+        
+            default:
+                arrayAvanco[0].classList.remove("ativo");
+                arrayAvanco[1].classList.remove("ativo");
+                arrayAvanco[2].classList.remove("ativo");
+                arrayAvanco[3].classList.remove("ativo");
+                break;
+        }
+    }
+
 }
-
-let jogador1 = new Jogador ();
-
 
---
+++ javascript/main.js
@@ -0,0 +1,17 @@
+// controla jogadores
+const jogador1 = new Jogador();
+// const jogador2 = new Jogador();
+// const jogador3 = new Jogador();
+// const jogador4 = new Jogador();
+// const jogador5 = new Jogador();
+// const jogador6 = new Jogador();
+// const jogador7 = new Jogador();
+// const jogador8 = new Jogador();
+// const jogador9 = new Jogador();
+// const jogador10 = new Jogador();
+
+// controla as pontuações dos jogadores
+// controla o avanço dos jogadores
+// controla o nome dos times
+// controla e criar os jogadores
+// controla o fim de jogo
