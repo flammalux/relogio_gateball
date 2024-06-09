5ff9ad435af4cc96058fe283209057300ef3b334
Flamma <flamma.lux@gmail.com>
2024-06-09 14:55:41 -0300

    removidos comentarios de teste, convertidas todas as variáveis da classe para variáveis privadas e o método Mostratempo també foi definido como privado.

--- javascript/ContadorRegressivo.js
+++ javascript/ContadorRegressivo.js
@@ -1,53 +1,62 @@
 class ContadorRegressivo {
-// class ContadorRegressivo extends HTMLElement {
-    constructor (tempoEmMinutos) {
-        // super ();
-        this.tempo = tempoEmMinutos * 60; // tempo que vai ser diminuído em segundos
-        this.tempoDecorrido = this.tempo;
-        this.tempoDecorridoEmTexto = this.MostraTempo(this.tempo);
-        this.contagemRegressiva = null;
-        this.pausado = false;
-        this.finalizado = false;
+
+    #tempo;
+    #tempoDecorrido;
+    #tempoDecorridoEmTexto;
+    #contagemRegressiva;
+    #pausado;
+    #finalizado;
+    #elementoAlterado;
+
+    constructor (tempoEmMinutos, alterar) {
+        this.#tempo = tempoEmMinutos * 60; // tempo que vai ser diminuído em segundos
+        this.#tempoDecorrido = this.#tempo;
+        this.#tempoDecorridoEmTexto = this.#MostraTempo(this.#tempo);
+        this.#contagemRegressiva = null;
+        this.#pausado = false;
+        this.#finalizado = false;
+        this.#elementoAlterado = alterar;
     }
 
-    get contagem () {return this.tempoDecorridoEmTexto;}
-    get pausa () {return this.pausado;}
-    get fim () {return this.finalizado;}
+    get contagem () {return this.#tempoDecorridoEmTexto;}
+    get pausa () {return this.#pausado;}
+    get fim () {return this.#finalizado;}
 
     IniciaContagem() {
-        if (this.pausado == true) {
-            this.pausado = false;
+        if (this.#pausado == true) {
+            this.#pausado = false;
         }
 
-        this.contagemRegressiva = setInterval(() => {
-            this.tempoDecorrido--;
-            if (this.tempoDecorrido < 0) {
-                clearInterval(this.contagemRegressiva);
-                this.finalizado = true;
+        this.#contagemRegressiva = setInterval(() => {
+            this.#tempoDecorrido--;
+            if (this.#tempoDecorrido <= 0) {
+                clearInterval(this.#contagemRegressiva);
+                this.#finalizado = true;
+                this.#elementoAlterado.classList.remove('ativo');
             }
-            this.tempoDecorridoEmTexto = this.MostraTempo(this.tempoDecorrido);
+            this.#tempoDecorridoEmTexto = this.#MostraTempo(this.#tempoDecorrido);
+            this.#elementoAlterado.innerHTML = this.#tempoDecorridoEmTexto;
         }, 1000);
         return;
     }
 
     PausaContagem() {
-        this.pausado = true;
-        clearInterval(this.contagemRegressiva);
+        this.#pausado = true;
+        clearInterval(this.#contagemRegressiva);
         return;
     }
 
     ResetaContagem() {
-        clearInterval(this.contagemRegressiva);
-        this.tempoDecorrido = this.tempo;
-        this.tempoDecorridoEmTexto = "";
-        this.finalizado = false;
+        clearInterval(this.#contagemRegressiva);
+        this.#tempoDecorrido = this.#tempo;
+        this.#tempoDecorridoEmTexto = "";
+        this.#finalizado = false;
         return;
     }
 
-    MostraTempo (tempoEmSegundo) {
+    #MostraTempo (tempoEmSegundo) {
         const tempo = new Date(tempoEmSegundo * 1000);
         const tempoFormatado = tempo.toLocaleTimeString("pt-br", {minute: "2-digit", second: "2-digit"});
-        // console.log(tempoFormatado);
         return tempoFormatado;
     }
 }
